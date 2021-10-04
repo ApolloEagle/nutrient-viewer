bdd08262673241bfe02700dd5ab4d34e17180a7f
Blake Reimer <blakereimer@testeid3s-MacBook-Pro.local>
2021-10-04 16:12:53 -0600

    implement selected food list

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState } from "react";
+import round from 'lodash/round';
 import {
   TextField,
   Box,
@@ -7,33 +8,37 @@ import {
   List,
   ListItem,
   ListItemText,
-  InputAdornment,
   ListItemButton,
-  ListItemIcon,
-} from '@mui/material';
-import Search from '@mui/icons-material/Search';
+} from "@mui/material";
 
 function Content() {
   const apiKey = process.env.REACT_APP_API_KEY;
   const baseUrl = `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${apiKey}&query=`;
 
   const [foods, setFoods] = useState([]);
-  const [selectedFood, setSelectedFood] = useState('');
+  const [selectedFoods, setSelectedFoods] = useState([]);
+  const [renderList, setRenderList] = useState(false);
   const [nutrients, setNutrients] = useState([
-    { nutrientId: 1003, name: 'Protein', value: 0, unit: 'g' },
-    { nutrientId: 2000, name: 'Sugar', value: 0, unit: 'g' },
+    { nutrientId: 1003, name: "Protein", value: 0, unit: "g" },
+    { nutrientId: 2000, name: "Sugar", value: 0, unit: "g" },
   ]);
 
   const handleSearch = async (input) => {
-    setSelectedFood('');
-    const url = `${baseUrl}${input}`;
-    const res = await fetch(url);
-    const body = await res.json();
-    setFoods(body.foods);
+    setRenderList(false);
+    if (!!input) {
+      setRenderList(true);
+      const url = `${baseUrl}${input}`;
+      const res = await fetch(url);
+      const body = await res.json();
+
+      setFoods(body.foods);
+    } else {
+      setRenderList(false);
+    }
   };
 
   const handleSelect = async (input) => {
-    setSelectedFood(input.description);
+    setSelectedFoods([...selectedFoods, input]);
     const selectedNutrients = input.foodNutrients;
     let index = 0;
     selectedNutrients.forEach((nutrient) => {
@@ -42,7 +47,8 @@ function Content() {
       );
       if (index >= 0) {
         let updatedNutrients = [...nutrients];
-        updatedNutrients[index].value = nutrient.value;
+        updatedNutrients[index].value += nutrient.value;
+        updatedNutrients[index].value = round(updatedNutrients[index].value, 2)
         setNutrients(updatedNutrients);
       }
     });
@@ -55,11 +61,25 @@ function Content() {
       </Grid>
       <Grid item xs={5}>
         <List>
+          {!!selectedFoods &&
+            selectedFoods.map((selectedFood) => {
+              return (
+                <ListItem
+                  sx={{
+                    backgroundColor: "magenta",
+                  }}
+                >
+                  {selectedFood.description}
+                </ListItem>
+              );
+            })}
+        </List>
+        <List>
           <ListItem
             sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              backgroundColor: 'pink',
+              display: "flex",
+              flexDirection: "column",
+              backgroundColor: "pink",
             }}
           >
             <Box>
@@ -68,25 +88,20 @@ function Content() {
                 onChange={(e) => handleSearch(e.target.value)}
                 size="small"
                 fullWidth
-                placeholder="Search Eggs, Broccoli, etc."
+                placeholder="Search Eggs, Bread, etc."
                 InputProps={{
                   disableUnderline: true,
-                  // startAdornment: (
-                  //   <InputAdornment position="start">
-                  //     <Search fontSize="small" />
-                  //   </InputAdornment>
-                  // ),
                 }}
               />
-              {!selectedFood && (
+              {renderList && (
                 <List
                   sx={{
-                    position: 'relative',
-                    overflow: 'auto',
+                    position: "relative",
+                    overflow: "auto",
                     maxHeight: 100,
                   }}
                 >
-                  {foods.map((food, index) => {
+                  {foods?.map((food, index) => {
                     return (
                       <ListItemButton dense={true}>
                         <ListItem
@@ -106,7 +121,7 @@ function Content() {
       </Grid>
       <Grid item xs={7}>
         <List>
-          {nutrients.map((nutrient, index) => {
+          {!!nutrients && nutrients.map((nutrient, index) => {
             return (
               <ListItem key={index}>
                 <ListItemText
